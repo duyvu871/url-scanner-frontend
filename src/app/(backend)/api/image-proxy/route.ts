e366cbd4a4f3c9bd0869992458299be82d82e3a0
duyvu871 <zoocp123@gmail.com>
2024-09-23 10:48:51 +0700

    update sth

--- src/app/(backend)/api/image-proxy/route.ts
+++ src/app/(backend)/api/image-proxy/route.ts
@@ -1,13 +1,17 @@
 
 import { NextRequest, NextResponse } from 'next/server';
 
+export const maxDuration = 60;
+
+export const dynamic = 'force-dynamic'
+
 export async function GET(req: NextRequest) {
+    const query = req.nextUrl.searchParams;
+    const imageUrl = query.get('url');
+    if (!imageUrl) {
+        return new Response("Missing 'url' query parameter", { status: 400 });
+    }
     try {
-        const query = req.nextUrl.searchParams;
-        const imageUrl = query.get('url');
-        if (!imageUrl) {
-            return new Response("Missing 'url' query parameter", { status: 400 });
-        }
 
         // Fetch the image from the external source
         const response = await fetch(imageUrl);
